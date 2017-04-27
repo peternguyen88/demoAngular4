bc0c9111f2b62d8991b61ea731aa6e70aa09d822
peternguyen88 <tronghieubk@gmail.com>
2017-04-27 23:35:26 +0700

    Test Result Class

---
+++ Angular4_CLI_Starter/src/app/models/test-result.ts
@@ -0,0 +1,34 @@
+import {GMATTest} from "./gmat-test";
+import {Question} from "./question";
+
+export class TestResult{
+  constructor(test: GMATTest){
+    this.testName = test.testName;
+    this.numberOfQuestions = test.numberOfQuestions;
+    this.questions = [];
+    test.questions.filter(e => e.selected_answer!=null).forEach(e => {
+      this.numberOfAnsweredQuestions++;
+      this.questions.push(new QuestionResult(e));
+    });
+    this.isTestFinished = this.numberOfQuestions == this.numberOfAnsweredQuestions;
+  }
+
+  testName: string;
+  numberOfQuestions: number;
+  numberOfAnsweredQuestions
+  questions: QuestionResult[];
+  isTestFinished: boolean;
+}
+
+export class QuestionResult{
+  constructor(question: Question){
+    this.question_number = question.question_number;
+    this.selected_answer = question.selected_answer;
+    this.is_correct = question.isCorrect();
+    this.question_time = question.question_time;
+  }
+  question_number: number;
+  selected_answer: string;
+  question_time: number;
+  is_correct: boolean;
+}

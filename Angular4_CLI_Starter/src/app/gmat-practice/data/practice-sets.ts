73c728e8333bffd2b316a59a260f304ec4304738
Peter <tronghieubk@gmail.com>
2017-09-20 09:12:04 +0700

    Add Practice Full Name

--- Angular4_CLI_Starter/src/app/gmat-practice/data/practice-sets.ts
+++ Angular4_CLI_Starter/src/app/gmat-practice/data/practice-sets.ts
@@ -4,32 +4,32 @@ import {Question} from "../../models/question";
 export class PracticeData {
   // Structure: 'practice name', 'number of questions', 'status', 'link to file', 'last time of major change in file'
   static DATA = [
-    ['OG15-CR',124,Status.ACTIVE, 'assets/practices/OG15/cr.txt',0],
-    ['OG15-SC',140,Status.ACTIVE, 'assets/practices/OG15/sc.txt',0],
-    ['OG15-RC',144,Status.ACTIVE, 'assets/practices/OG15/rc.txt',0],
-    ['VR15-CR',83,Status.ACTIVE, 'assets/practices/VR15/cr.txt',0],
-    ['VR15-SC',144,Status.ACTIVE, 'assets/practices/VR15/sc.txt',0],
-    ['VR15-RC',104,Status.ACTIVE, 'assets/practices/VR15/rc.txt',0],
-    ['QP1-CR',75,Status.ACTIVE, 'assets/practices/QP1/cr.txt',0],
-    ['QP1-SC',75,Status.ACTIVE, 'assets/practices/QP1/sc.txt',0],
-    ['QP1-RC',75,Status.ACTIVE, 'assets/practices/QP1/rc.txt',0]
+    ['OG15-CR',124,Status.ACTIVE, 'assets/practices/OG15/cr.txt',0, 'Official Guide 2015 - CR'],
+    ['OG15-SC',140,Status.ACTIVE, 'assets/practices/OG15/sc.txt',0, 'Official Guide 2015 - SC'],
+    ['OG15-RC',144,Status.ACTIVE, 'assets/practices/OG15/rc.txt',0, 'Official Guide 2015 - RC'],
+    ['VR15-CR',83,Status.ACTIVE, 'assets/practices/VR15/cr.txt',0, 'Verbal Review 2015 - CR'],
+    ['VR15-SC',144,Status.ACTIVE, 'assets/practices/VR15/sc.txt',0, 'Verbal Review 2015 - SC'],
+    ['VR15-RC',104,Status.ACTIVE, 'assets/practices/VR15/rc.txt',0, 'Verbal Review 2015 - RC'],
+    ['QP1-CR',75,Status.ACTIVE, 'assets/practices/QP1/cr.txt',0, 'Question Pack 1 - CR'],
+    ['QP1-SC',75,Status.ACTIVE, 'assets/practices/QP1/sc.txt',0, 'Question Pack 1 - SC'],
+    ['QP1-RC',75,Status.ACTIVE, 'assets/practices/QP1/rc.txt',0, 'Question Pack 1 - RC']
   ];
 
   static PREMIUM_DATA = [
-    ['OG16',102,Status.ACTIVE, 'assets/premium/og16.txt',0],
+    ['OG16',102,Status.ACTIVE, 'assets/premium/og16.txt',0, 'Official Guide 2016'],
     ['VR16',76,Status.ACTIVE, 'assets/premium/vr16.txt',0, 'Verbal Review 2016'],
-    ['OG17',61,Status.ACTIVE, 'assets/premium/og17.txt',0],
-    ['VR17',45,Status.ACTIVE, 'assets/premium/vr17.txt',0],
-    ['OG18',61,Status.ACTIVE, 'assets/premium/og18.txt',0],
-    ['VR18',42,Status.ACTIVE, 'assets/premium/vr18.txt',0],
+    ['OG17',61,Status.ACTIVE, 'assets/premium/og17.txt',0, 'Official Guide 2017'],
+    ['VR17',45,Status.ACTIVE, 'assets/premium/vr17.txt',0, 'Verbal Review 2017'],
+    ['OG18',61,Status.ACTIVE, 'assets/premium/og18.txt',0, 'Official Guide 2018'],
+    ['VR18',42,Status.ACTIVE, 'assets/premium/vr18.txt',0, 'Verbal Review 2018'],
   ];
 
   static COMPREHENSIVE_SC = [
-    ['COM-SC-01',84,Status.ACTIVE, 'assets/comprehensive/sc_01.txt',0],
-    ['COM-SC-02',84,Status.ACTIVE, 'assets/comprehensive/sc_02.txt',0],
-    ['COM-SC-03',84,Status.ACTIVE, 'assets/comprehensive/sc_03.txt',0],
-    ['COM-SC-04',84,Status.ACTIVE, 'assets/comprehensive/sc_04.txt',0],
-    ['COM-SC-05',83,Status.ACTIVE, 'assets/comprehensive/sc_05.txt',0],
+    ['COM-SC-01',84,Status.ACTIVE, 'assets/comprehensive/sc_01.txt',0, 'SC Comprehensive - Part I'],
+    ['COM-SC-02',84,Status.ACTIVE, 'assets/comprehensive/sc_02.txt',0, 'SC Comprehensive - Part II'],
+    ['COM-SC-03',84,Status.ACTIVE, 'assets/comprehensive/sc_03.txt',0, 'SC Comprehensive - Part III'],
+    ['COM-SC-04',84,Status.ACTIVE, 'assets/comprehensive/sc_04.txt',0, 'SC Comprehensive - Part IV'],
+    ['COM-SC-05',83,Status.ACTIVE, 'assets/comprehensive/sc_05.txt',0, 'SC Comprehensive - Part V'],
   ];
 
   static practices:GMATPractice[];
